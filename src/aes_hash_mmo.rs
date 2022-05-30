e59e505cd46439e55935558d2f4c6a22c0e190f2
Theophile BREZOT <theophile.brezot@vulpecula.fr>
2022-05-30 17:07:23 +0200

    Review AES MMO hash

--- src/aes_hash_mmo.rs
+++ src/aes_hash_mmo.rs
@@ -3,32 +3,32 @@ use aes::{
     Aes256,
 };
 
+pub const BLOCK_SIZE: usize = 32;
+
 /// Using AES 256 as a hash function.
 /// Implements the scheme of  S. Matyas, C. Meyer and J. Oseas
 /// Hᵢ = E(Hᵢ₋₁, Xᵢ)^Xᵢ^Hᵢ₋₁ where the AES
 /// encryption of plaintext X with key K will is denoted with E(K, X)
 /// see `<https://www.esat.kuleuven.be/cosic/publications/article-48.pdf>`
-/// Since the block size is 16 and we need to encrypt 32 bytes (top get a 256
+/// Since the block size is 16 and we need to encrypt 32 bytes (to get a 256
 /// bit hash) we use AES in counter mode to encrypt two blocks of 16 bytes
 pub struct AesMmo {
-    hash: [u8; 32],
-    block: [u8; 32],
+    hash: [u8; BLOCK_SIZE],
+    block: [u8; BLOCK_SIZE],
     block_offset: usize,
 }
 
 /// This is the routine implementing: Hᵢ = E(Hᵢ₋₁, Xᵢ)^Xᵢ^Hᵢ₋
 /// The block must be 32 byte long
 #[inline]
-fn hash_block(block: &[u8; 32], hash: &mut [u8; 32]) {
-    let mut b = [0_u8; 32];
-    b.copy_from_slice(block);
+fn hash_block(block: &[u8; BLOCK_SIZE], hash: &mut [u8; 32]) {
     // since AES blocks are only 16 bytes long, we split the data to hash in 2
     let mut block_16 = GenericArray::clone_from_slice(&block[0..16]);
-    // first 16 block
     let cipher = Aes256::new(GenericArray::from_slice(hash));
     cipher.encrypt_block(&mut block_16);
     for i in 0..16 {
         // calculate the new hash according to MMO
+        // the last XOR operation is used since we call AES two times per block
         hash[i] = block_16[i] ^ block[i] ^ hash[i + 16];
         // put last 16 bytes of input in block
         block_16[i] = block[i + 16];
@@ -36,7 +36,7 @@ fn hash_block(block: &[u8; 32], hash: &mut [u8; 32]) {
     // second 16 byte block - the "key" is now made of Hᵢ₋₁||Hᵢ₋₂
     let cipher = Aes256::new(GenericArray::from_slice(hash));
     cipher.encrypt_block(&mut block_16);
-    for i in 16..32 {
+    for i in 16..BLOCK_SIZE {
         hash[i] = block_16[i - 16] ^ block[i] ^ hash[i - 16];
     }
 }
@@ -51,10 +51,10 @@ impl AesMmo {
     }
 
     #[must_use]
-    pub const fn new(seed: &[u8; 32]) -> Self {
+    pub fn new(seed: &[u8; BLOCK_SIZE]) -> Self {
         Self {
             hash: *seed,
-            block: [0_u8; 32],
+            block: [0; BLOCK_SIZE],
             block_offset: 0,
         }
     }
@@ -62,45 +62,43 @@ impl AesMmo {
     /// Digest data, updating the internal state.
     /// This method can be called repeatedly for use with streaming messages.
     pub fn update(&mut self, data: &[u8]) {
-        let data_len = data.len();
-        let mut data_offset = 0_usize;
+        let mut data_offset = 0;
 
         loop {
-            let data_left = data_len - data_offset;
-            let data_to_copy = if data_left >= 32 - self.block_offset {
-                32 - self.block_offset
-            } else {
-                data_left
-            };
+            // get the number of bytes available to try to fill a block
+            let data_to_copy = usize::min(data.len() - data_offset, BLOCK_SIZE - self.block_offset);
+
+            // update the block data and offset
             self.block[self.block_offset..self.block_offset + data_to_copy]
                 .copy_from_slice(&data[data_offset..data_offset + data_to_copy]);
-            data_offset += data_to_copy;
             self.block_offset += data_to_copy;
-            if self.block_offset < 32 {
+
+            // stop there if there is not enough data to fill a block
+            if self.block_offset < BLOCK_SIZE {
                 return;
             }
+
+            // hash block and update offsets
             hash_block(&self.block, &mut self.hash);
-            // reset block
+            data_offset += data_to_copy;
             self.block_offset = 0;
-            self.block.copy_from_slice(&[0_u8; 32]);
         }
     }
 
     /// Retrieve result and consume hasher instance.
     #[must_use]
-    pub fn finalize(&self) -> [u8; 32] {
-        let mut hash = [0_u8; 32];
-        hash.copy_from_slice(&self.hash);
+    pub fn finalize(mut self) -> [u8; BLOCK_SIZE] {
+        // hash remaining data by padding with 0s
         if self.block_offset > 0 {
-            hash_block(&self.block, &mut hash);
+            self.update(&[0; 32]);
         }
-        hash
+        self.hash
     }
 
     /// Convenience function to compute hash of the data.
     /// It will handle hasher creation, data feeding and finalization.
     #[must_use]
-    pub fn digest(seed: &[u8; 32], data: &[u8]) -> [u8; 32] {
+    pub fn digest(seed: &[u8; BLOCK_SIZE], data: &[u8]) -> [u8; BLOCK_SIZE] {
         let mut hasher = Self::new(seed);
         hasher.update(data);
         hasher.finalize()
@@ -109,10 +107,8 @@ impl AesMmo {
     /// Convenience function to compute 2 hashes of the data.
     /// It will handle hasher creation, data feeding and finalization.
     #[must_use]
-    pub fn digest2(seed: &[u8; 32], data: &[u8]) -> ([u8; 32], [u8; 32]) {
-        let mut hasher = Self::new(seed);
-        hasher.update(data);
-        let first_hash = hasher.finalize();
+    pub fn digest2(seed: &[u8; BLOCK_SIZE], data: &[u8]) -> ([u8; BLOCK_SIZE], [u8; BLOCK_SIZE]) {
+        let first_hash = Self::digest(seed, data);
         let mut second_hash = first_hash;
         hash_block(&first_hash, &mut second_hash);
         assert_ne!(first_hash, second_hash);
@@ -120,9 +116,8 @@ impl AesMmo {
     }
 }
 
-#[cfg(all(test, feature = "libsodium"))]
+#[cfg(test)]
 pub(crate) mod tests {
-    use std::time::Instant;
 
     use rand::{prelude::*, RngCore, SeedableRng};
     use rand_hc::Hc128Rng;
@@ -146,6 +141,7 @@ pub(crate) mod tests {
 
     #[test]
     fn test_small_hash() {
+        println!("{}", AesMmo::is_available());
         let mut cs_rng = Hc128Rng::from_entropy();
         let mut seed = [0_u8; 32];
         cs_rng.fill_bytes(&mut seed);
@@ -190,51 +186,75 @@ pub(crate) mod tests {
         }
     }
 
-    #[test]
-    #[ignore = "too slow for CI"]
-    #[allow(unused_must_use)]
-    fn bench_hash() {
-        let mut cs_rng = Hc128Rng::from_entropy();
+    #[cfg(feature = "libsodium")]
+    mod sodium_tests {
 
-        // generate the seed
-        let mut seed = [0_u8; 32];
-        cs_rng.fill_bytes(&mut seed);
+        use std::time::Instant;
 
-        for l in 1..17_u64 {
-            // generate random data
-            let data_len = 16 * l;
-            let mut data = vec![0_u8; data_len as usize];
-            cs_rng.fill_bytes(&mut data);
+        use super::*;
+        use crate::sodium_bindings;
 
-            let rounds = 500_000_u128;
-            let mut nanos_aes = 0_u128;
-            let mut nanos_sha256 = 0_u128;
-            let mut nanos_blake = 0_u128;
-            for _i in 0..rounds {
-                let now = Instant::now();
-                AesMmo::digest(&seed, &data);
-                nanos_aes += now.elapsed().as_nanos();
-                // compare with sha256 from sodium bindings
-                let mut h = [0_u8; 32];
-                let now = Instant::now();
-                unsafe {
-                    sodium_bindings::crypto_hash_sha256(h.as_mut_ptr(), data.as_ptr(), data_len)
-                };
-                nanos_sha256 += now.elapsed().as_nanos();
-                // Blake2b
-                let now = Instant::now();
-                blake2b(&seed, &data);
-                nanos_blake += now.elapsed().as_nanos();
+        pub(crate) fn blake2b(seed: &[u8; 32], data: &[u8]) -> [u8; 32] {
+            let mut h = [0_u8; 32];
+            unsafe {
+                sodium_bindings::crypto_generichash_blake2b(
+                    h.as_mut_ptr(),
+                    32,
+                    data.as_ptr(),
+                    data.len() as u64,
+                    seed.as_ptr(),
+                    32,
+                )
+            };
+            h
+        }
+
+        #[test]
+        #[ignore = "too slow for CI"]
+        #[allow(unused_must_use)]
+        fn bench_hash() {
+            let mut cs_rng = Hc128Rng::from_entropy();
+
+            // generate the seed
+            let mut seed = [0_u8; 32];
+            cs_rng.fill_bytes(&mut seed);
+
+            for l in 1..17_u64 {
+                // generate random data
+                let data_len = 16 * l;
+                let mut data = vec![0_u8; data_len as usize];
+                cs_rng.fill_bytes(&mut data);
+
+                let rounds = 500_000_u128;
+                let mut nanos_aes = 0_u128;
+                let mut nanos_sha256 = 0_u128;
+                let mut nanos_blake = 0_u128;
+                for _i in 0..rounds {
+                    let now = Instant::now();
+                    AesMmo::digest(&seed, &data);
+                    nanos_aes += now.elapsed().as_nanos();
+                    // compare with sha256 from sodium bindings
+                    let mut h = [0_u8; 32];
+                    let now = Instant::now();
+                    unsafe {
+                        sodium_bindings::crypto_hash_sha256(h.as_mut_ptr(), data.as_ptr(), data_len)
+                    };
+                    nanos_sha256 += now.elapsed().as_nanos();
+                    // Blake2b
+                    let now = Instant::now();
+                    blake2b(&seed, &data);
+                    nanos_blake += now.elapsed().as_nanos();
+                }
+                println!(
+                    "Average over {} rounds of {} data bytes: nano per hash aes: {}; sha256 {}; \
+                     blake: {}",
+                    rounds,
+                    16 * l,
+                    nanos_aes / rounds,
+                    nanos_sha256 / rounds,
+                    nanos_blake / rounds,
+                )
             }
-            println!(
-                "Average over {} rounds of {} data bytes: nano per hash aes: {}; sha256 {}; \
-                 blake: {}",
-                rounds,
-                16 * l,
-                nanos_aes / rounds,
-                nanos_sha256 / rounds,
-                nanos_blake / rounds,
-            )
         }
     }
 }
