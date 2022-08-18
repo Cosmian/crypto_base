7e6a743c0206e86bd6fa395867995741ace8decd
Theophile BREZOT <theophile.brezot@cosmian.com>
2022-08-18 15:10:52 +0200

    Export `BytesScanner`

--- src/symmetric_crypto/mod.rs
+++ src/symmetric_crypto/mod.rs
@@ -4,7 +4,7 @@ pub mod aes_256_gcm_sodium;
 pub mod xchacha20;
 
 pub use cosmian_crypto_core::symmetric_crypto::{
-    aes_256_gcm_pure, nonce, Block, Dem, Metadata, SymmetricCrypto,
+    aes_256_gcm_pure, nonce, Block, BytesScanner, Dem, Metadata, SymKey, SymmetricCrypto,
 };
 
 pub const MIN_DATA_LENGTH: usize = 1;
