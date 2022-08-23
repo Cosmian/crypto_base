bf6971d1b85177d70e5c1b7ecb705a06a01f50ce
Theophile BREZOT <theophile.brezot@cosmian.com>
2022-08-23 15:32:14 +0200

    Add `cargo audit` to GitHub CIs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod error;
+
 pub mod aes_hash_mmo;
 pub mod asymmetric;
 pub mod brc_c_prf_hi;
@@ -14,31 +16,9 @@ pub mod primes;
 #[cfg(all(not(target_arch = "wasm32"), not(windows), feature = "libsodium"))]
 pub mod sodium_bindings;
 
-use std::array::TryFromSliceError;
-
 pub use cosmian_crypto_core::{
     entropy, kdf,
     reexport::generic_array::{typenum, ArrayLength, GenericArray},
     CryptoCoreError, KeyTrait,
 };
-
-use thiserror::Error;
-#[derive(Debug, Error, PartialEq)]
-pub enum CryptoBaseError {
-    #[error("{0}")]
-    CryptoCoreError(#[from] CryptoCoreError),
-    #[error("Failed to parse")]
-    HexParseError(#[from] hex::FromHexError),
-    #[error("Conversion failed: {0}")]
-    ConversionFailed(String),
-    #[error("Wrong size: {given} given should be {expected}")]
-    SizeError { given: usize, expected: usize },
-    #[error("Invalid size")]
-    InvalidSize(String),
-}
-
-impl From<TryFromSliceError> for CryptoBaseError {
-    fn from(e: TryFromSliceError) -> Self {
-        Self::ConversionFailed(e.to_string())
-    }
-}
+pub use error::CryptoBaseError;
